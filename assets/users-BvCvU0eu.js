e6887da30c2412e7639c97b79ad02de6f8b16471
p09057 <p09057@users.noreply.github.com>
2025-07-18 23:16:11 +0900

    Updates

--- assets/users-BvCvU0eu.js
+++
@@ -1,1 +0,0 @@
-const a=[{id:"tshirai",username:"tshirai",passwordHash:"53ab39b7",role:"admin"},{id:"hiroshi",username:"hiroshi",passwordHash:"3edf1206",role:"admin"},{id:"tomoko",username:"tomoko",passwordHash:"6dbe6d77",role:"admin"},{id:"atobe",username:"atobe",passwordHash:"40419a51",role:"admin"}];export{a as USERS};
